1970409fca82cbdbd0e55f746eb3b97b50ffbf32
Nicole Provenza <nicoleprovenza@gmail.com>
2023-06-05 11:13:09 -0500

    code updates

---
+++ utils/decibelize.m
@@ -0,0 +1,13 @@
+function logScaledData = decibelize(filledRawData)
+%% For Visualization as a template: Log Mean Normalized Data, Divide By Standard Deviation
+logScaledData  = {};
+temp = 10*log10(filledRawData./mean(filledRawData,1,'omitnan'));
+logScaledData = temp./std(temp,0,'omitnan');
+end
+
+
+
+
+
+
+
