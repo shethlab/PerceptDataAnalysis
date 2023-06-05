97951a53e68ac6661f0806ba7617755d63e1e719
Sandy Reddy <Sandesh.Reddy@bcm.edu>
2023-06-05 10:40:19 -0500

    Update fillData.m
    Avoid daily edge effects and replace outliers

--- utils/fillData.m
+++ utils/fillData.m
@@ -1,10 +1,20 @@
-function filledData = fillData(matrix)
+function filledData = fillData(matrix,days)
 filledData = [];
-for q = 1:width(matrix)
-    d = matrix(:,q);
-    if length(find(isnan(d))) <3
-        filledData(:,q) = fillmissing(d,'pchip');
-    else
-        filledData(:,q) = d;
-    end
+
+start_index=find(diff(days)>1);
+try
+    start_index=[1,start_index+1,length(days)+1];
+catch
+    start_index=[1,length(days)+1];
+end
+
+comb_1d=[];
+for i=1:length(start_index)-1
+    matrix1d=reshape(matrix(:,start_index(i):start_index(i+1)-1),[1,144*(start_index(i+1)-start_index(i))]);
+    matrix1d=filloutliers(matrix1d,'pchip','mean','ThresholdFactor',10);
+    matrix1d=fillmissing(matrix1d,'pchip','MaxGap',7);  
+    comb_1d=[comb_1d,matrix1d];
+end
+
+filledData=reshape(comb_1d,[size(matrix)]);
 end
