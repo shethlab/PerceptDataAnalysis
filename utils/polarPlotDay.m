4507ec8024c5fa940282ecca7b7120cec0e0bd5b
Nicole Provenza <nicoleprovenza@gmail.com>
2023-06-05 11:43:39 -0500

    Update polarPlotDay.m

--- utils/polarPlotDay.m
+++ utils/polarPlotDay.m
@@ -43,6 +43,6 @@ if size(day,2) ==1
     thetaticklabels({});
     pax.RTickLabels = {};
     %thetaticklabels({'0:00','2:00','4:00','6:00','8:00','10:00','12:00','14:00','16:00','18:00','20:00','22:00'})
-    title(titlestring);
+    title(titlestring,'Units', 'normalized',Position=[0.5, -0.1, 0]);
 end
 end
