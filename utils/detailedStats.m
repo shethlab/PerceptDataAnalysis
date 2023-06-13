69c13d1f3e139efe46983f34482e91064300a44c
Sandy Reddy <Sandesh.Reddy@bcm.edu>
2023-06-13 00:55:55 -0500

    Multiple corrections and expanded variables
    Deals with missing inputs, expands effect size into tables, switched effect size to Cohen D

--- utils/detailedStats.m
+++ utils/detailedStats.m
@@ -1,10 +1,26 @@
 function allStats = detailedStats(x,y)
 %% Return Detailed Statistics obtained from testing H0 that x ~ y
-[~,p,ci,stats] = ttest2(x,y,'Vartype','unequal');
-effect = meanEffectSize(x,y,'VarianceType','unequal');
-allStats.pvalue = p;
-allStats.CI = ci;
-allStats.tStat = stats.tstat;
-allStats.df = stats.df;
-allStats.effectSize = effect;
+if ~isempty(x) && ~isempty(y)
+    [~,p,ci,stats] = ttest2(x,y,'Vartype','unequal');
+    effect = meanEffectSize(x,y,'Effect','cohen','VarianceType','unequal');
+    if p*3 >= 1
+        allStats.pvalue = 1;
+    else
+        allStats.pvalue = p*3;
+    end
+    allStats.tStat = stats.tstat;
+    allStats.CI = ci;
+    allStats.df = stats.df;
+    allStats.effectSize = effect.Effect;
+    allStats.effectCI = effect.ConfidenceIntervals;
+else
+    allStats.pvalue = [];
+    allStats.tStat = [];
+    allStats.CI = [];
+    allStats.df = [];
+    allStats.effectSize = [];
+    allStats.effectCI = [];
+end
+    allStats.sampleSizePre = length(y);
+    allStats.sampleSizePost = length(x);
 end
