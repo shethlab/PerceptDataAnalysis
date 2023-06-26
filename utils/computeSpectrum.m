90933fb447dcc6846fa6055201eaa9fcf6a17099
skywalker1git <sameervikrajesh@gmail.com>
2023-06-27 00:47:57 +0530

    Update computeSpectrum.m

--- utils/computeSpectrum.m
+++ utils/computeSpectrum.m
@@ -4,15 +4,15 @@
 
 function [LFPamplitude,frequency] = computeSpectrum(raw)
 
-%% Conversion Factor
+%% Conversion Factor and Fs
 factor = rms(hanning(250))*2*sqrt(2)*250/256;
-%% Scale Time Domain
-working = raw*factor;
 fs = 250;
-
 %% Compute the Spectrum at the frequencies using a 256 point offline FFT (at fs = 250, this gives roughly 1Hz bin sizes)
-[psd,frequency] = pwelch(working,fs,fs*0.6,256,fs);
+[psd,frequency] = pwelch(raw,fs,fs*0.6,256,fs);
 
 %% Return the Square root of spectrum for LFP amplitude
-LFPamplitude = psd.^0.5;
+LFPamplitude = psd.^0.5*factor;
+
+
 end
+
