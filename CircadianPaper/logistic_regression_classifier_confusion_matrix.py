09f9490501e4dfd664dc9e8c944ddf9325f5aaaa
Racer2055 <71115591+Racer2055@users.noreply.github.com>
2023-07-14 11:03:17 -0500

    Update logistic_regression_classifier_confusion_matrix.py

--- CircadianPaper/logistic_regression_classifier_confusion_matrix.py
+++ CircadianPaper/logistic_regression_classifier_confusion_matrix.py
@@ -8,15 +8,18 @@ from sklearn.linear_model import LogisticRegression
 import EntropyHub as EH
 from statistics import mean, stdev
 
+# Function to create a DataFrame from .mat files
 def create_dataframe(mat_file, eng, num):
     time = [datetime.time(i // 60, i % 60) for i in range(0,1440,10)]
     df = pd.DataFrame(eng.eval(f"load('{mat_file}').filled{{{num},2}};"), index= time, columns= eng.eval(f"load('VCVS_all_filled.mat').comb_days{{{num},1}};")[0])
     return df
 
+# Function to add group labels to the DataFrame
 def add_group_labels(df, group_labels):
     df.loc['Group'] = group_labels
     return df
 
+# Function to calculate Sample Entropy for each group in the DataFrame
 def calculate_sample_entropy(df, pt_name, m, r, t, l, groups):
     pt = df.T
     sampEn_values = []
@@ -26,6 +29,7 @@ def calculate_sample_entropy(df, pt_name, m, r, t, l, groups):
             sampEn_values.append((EH.SampEn(data.iloc[day,:].to_numpy(), m = m, r = r, tau = t, Logx = l)[0][2], pt_name, group))
     return sampEn_values
 
+# Function to perform Logistic Regression using K-fold cross validation
 def perform_logistic_regression(X, y):
     X = X.reshape(-1,1)
     cv = KFold(n_splits = y.shape[0], shuffle=True)
@@ -42,35 +46,59 @@ def perform_logistic_regression(X, y):
         pred_rd.extend(clf.predict(test_X))
     return true, pred, pred_rd
 
+# Hemisphere option
+hemi = 0
+
 def main():
+    # Start a Matlab session
     eng = matlab.engine.start_matlab()
 
-    pt_numbers = [102, 105, 101, 103, 104]
-    id_num = range(1,6)
-    group_labels = [[0]*49 + [1]*48 + [3]*53, 
-                    [0]*7 + [1]*16 + [2]*15 + [1]*61,
-                    [0]*10 + [1]*9 + [2]*69 + [3]*186,
-                    [0]*45 + [1]*5 + [2]*90 + [3]*179,
-                    [0]*14 + [1]*187]
-    classifier_groups = [[0,3],[0,1],[0,3],[0,3],[0,1]]
+    if hemi == 0:
+        # Define patients' numbers, their corresponding IDs, group labels and groups for classification for the left hemisphere
+        # These will vary depending on the specific application
+        pt_numbers = [1, 2, 4, 5, 6]
+        id_num = range(1,6)
+        group_labels = [[0]*49 + [1]*48 + [3]*53, 
+                        [0]*7 + [1]*16 + [2]*15 + [1]*61,
+                        [0]*10 + [1]*9 + [2]*69 + [3]*186,
+                        [0]*45 + [1]*5 + [2]*90 + [3]*179,
+                        [0]*14 + [1]*187]
+        classifier_groups = [[0,3],[0,1],[0,3],[0,3],[0,1]]
+
+        responders = ['PT004', 'PT001', 'PT005']
+    else:
+        # Same as above, but for the right hemisphere
+        pt_numbers = [1, 2, 5, 6]
+        id_num = [1,2,4,5]
+        group_labels = [[0]*49 + [1]*48 + [3]*53, 
+                        [0]*7 + [1]*28 + [2]*15 + [1]*160,
+                        [0]*45 + [1]*5 + [2]*90 + [3]*179,
+                        [0]*14 + [1]*270]
+        
+        classifier_groups = [[0,3],[0,1],[0,3],[0,1]]
+
+        responders = ['PT001', 'PT005']
 
     m, r, t, l = 2, 3.6, 1, np.exp(1)
 
     sampEn_values = []
     
+    # Loop through each patient number, group label, classification group, and ID
     for num, group_label, class_group, id in zip(pt_numbers, group_labels, classifier_groups, id_num):
-        df = create_dataframe('filled.mat', eng, id)
-        df = add_group_labels(df, group_label)
-        sampEn_values.extend(calculate_sample_entropy(df, f'PT{num}', m, r, t, l, class_group))
+        df = create_dataframe('filled.mat', eng, id)  # Create the DataFrame from .mat files
+        df = add_group_labels(df, group_label)  # Add group labels to the DataFrame
+        sampEn_values.extend(calculate_sample_entropy(df, f'PT00{num}', m, r, t, l, class_group))  # Calculate Sample Entropy
 
     sampEn_values_df = pd.DataFrame(sampEn_values, columns=['Sample Entropy', 'PT', 'Group'])
     
+    # Placeholder lists for storing performance metrics
     sen = []
     fpr = []
     fnr = []
     spec = []
 
-    for pt_name in ['PT101', 'PT102', 'PT103']:
+    # Loop through each responder to calculate and print performance metrics
+    for pt_name in responders:
                 
         df = sampEn_values_df[sampEn_values_df['PT'] == pt_name]
         X = df['Sample Entropy'].to_numpy()
@@ -84,20 +112,21 @@ def main():
         fnr.append(fn/(tp+fn))
         spec.append(tn/(fp+tn))
         
-
-        
+    # Print performance metrics for the responders
     print(f'               Responders')
     print(f'Sensitivity: {round(mean(sen),3)} STD: {round(stdev(sen),3)}')
     print(f'False Positive Rate: {round(mean(fpr),3)} STD: {round(stdev(fpr),3)}')
     print(f'False Negative Rate: {round(mean(fnr),3)} STD: {round(stdev(fnr),3)}')
     print(f'Specificity: {round(mean(spec),3)} STD: {round(stdev(spec),3)}')
-    
+
+    # Reset placeholder lists for non-responders
     sen = []
     fpr = []
     fnr = []
     spec = []
 
-    for pt_name in ['PT105', 'PT104']:
+    # Loop through each non-responder to calculate and print performance metrics
+    for pt_name in ['PT002', 'PT006']:
                 
         df = sampEn_values_df[sampEn_values_df['PT'] == pt_name]
         X = df['Sample Entropy'].to_numpy()
@@ -105,17 +134,18 @@ def main():
         true, pred, pred_rd = perform_logistic_regression(X, y)
         
         tn, fp, fn, tp = confusion_matrix(true,pred_rd).ravel()
-        
+
         sen.append(tp/(tp+fn))
         fpr.append(fp/(fp+tn))
         fnr.append(fn/(tp+fn))
         spec.append(tn/(fp+tn))
         
-    print(f'\n\n               Non - Responders')
+    # Print performance metrics for the non-responders
+    print(f'               Non-Responders')
     print(f'Sensitivity: {round(mean(sen),3)} STD: {round(stdev(sen),3)}')
     print(f'False Positive Rate: {round(mean(fpr),3)} STD: {round(stdev(fpr),3)}')
     print(f'False Negative Rate: {round(mean(fnr),3)} STD: {round(stdev(fnr),3)}')
     print(f'Specificity: {round(mean(spec),3)} STD: {round(stdev(spec),3)}')
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
